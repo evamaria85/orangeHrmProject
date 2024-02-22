8a3289347f9d9dfdf59d725e6e5411d7dd32bdcf
hamza <hamza.ouhadda@gmail.com>
2024-02-21 23:49:25 -0600

    adding more test

--- orangHrmProject/src/main/java/pages/DashboardPage.java
+++ orangHrmProject/src/main/java/pages/DashboardPage.java
@@ -8,9 +8,17 @@ import org.openqa.selenium.support.PageFactory;
 public class DashboardPage {
     @FindBy(linkText = "Recruitment")private WebElement recruitmentLink;
 
+
+    @FindBy(linkText = "Admin") private WebElement adminLink;
+
     public RecruitmentPage navigateToRecruitmentPage(){
         recruitmentLink.click();
         return PageFactory.initElements(Browser.driver,RecruitmentPage.class);
+    }
+    public AdminPage navigateToAdminPage(String s){
+        adminLink.click();
+        return PageFactory.initElements(Browser.driver,AdminPage.class);
+
     }
 
 }
